64c1212f2a6482f4f5fb1222d9ac8a40269dc049
l3onwu <leonwudev@gmail.com>
2022-07-31 22:15:43 +1000

    Feat: Add skyshot project link

--- components/ProjectSection.tsx
+++ components/ProjectSection.tsx
@@ -1,27 +1,53 @@
 import SectionHeading from "./SectionHeading";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Stack, Text } from "@chakra-ui/react";
+import { FaRainbow } from "react-icons/fa";
+import { LinkBox, LinkOverlay } from "@chakra-ui/react";
 
 export default function ProjectSection() {
   return (
     <>
       {/* Header */}
-      <Box mb="20px">
+      <Box mb="30px">
         <SectionHeading>Projects</SectionHeading>
       </Box>
 
       {/* Projects grid */}
+
+      {/* Skyshot */}
       <Flex direction="row" wrap="wrap">
-        <Box
-          px="80px"
-          py="50px"
-          bgColor="whitesmoke"
-          textAlign="center"
-          mr="20px"
-          mb="20px"
-          borderRadius="20px"
-        >
-          Coming soon
-        </Box>
+        <LinkBox>
+          <Box
+            px="40px"
+            py="40px"
+            textAlign="center"
+            mr="20px"
+            mb="20px"
+            borderRadius="20px"
+            bgColor="#171819"
+            _hover={{
+              cursor: "pointer",
+              background: "#3181CE",
+              textStyle: "none",
+            }}
+            transition="0.2s background ease-in"
+          >
+            <LinkOverlay href={"https://skyshot.vercel.app/"} isExternal />
+            <Stack direction="row" align="center">
+              <FaRainbow color="white" />
+              <Text
+                color="white"
+                fontWeight="bold"
+                fontFamily="Oswald"
+                fontSize="24px"
+              >
+                SKYSHOT
+              </Text>
+            </Stack>
+            <Text color="white" fontSize="14px">
+              Snapshot of your weather
+            </Text>
+          </Box>
+        </LinkBox>
       </Flex>
     </>
   );
